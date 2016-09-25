0f3f6e9e6797bbc67b813bc6cade133891209d20
Pat Jacobs <musicbender@gmail.com>
2016-09-24 19:54:05 -0700

    created component that displays text as it is typed

--- src/index.jsx
+++ src/index.jsx
@@ -1,34 +1,32 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-var Dropdown = require('./components/Dropdown.jsx');
-var View = require('./components/View.jsx');
-
-require('./style/base.scss');
+import React from 'react';
+import { render } from 'react-dom';
 
 var Container = React.createClass({
     getInitialState: function () {
         return {
-            name: 'Lea'
+            userInput: ''
         }
     },
     
-    changeName: function (newName) {
+    handleInputChange: function (e) {
         this.setState({
-            name: newName
+            userInput: e.target.value
         })
     },
     
     render: function () {
         return (
             <div>
-                <h1>Baby Name Tester</h1>
-                <Dropdown onChange={this.changeName} />
-                <View name={this.state.name} />
-            </div>
+                <h1>Text Reverser</h1>
+                <input type="text" onChange={ this.handleInputChange } value={ this.state.userInput } />
+                <h2 className="output">{ this.state.userInput }</h2>
+            </div>  
         )
     }
 });
 
-ReactDOM.render(
-    <Container />, document.getElementById('app')
-); 
+render(<Container />, document.getElementById('app'));
+    
+
+
+
